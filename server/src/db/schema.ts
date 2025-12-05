ddaa9b3570f217a5466cc6ab1199ce236c5463da
Tobeker <t.wendenburg@yahoo.de>
2025-12-05 09:18:53 +0100

    chirp Tabelle auskomentiert

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -12,7 +12,7 @@ export const users = pgTable("users", {
   isChirpyRed: boolean("is_chirpy_red").notNull().default(false),
 });
 
-export const chirps = pgTable("chirps", {
+/*export const chirps = pgTable("chirps", {
   id: uuid("id").primaryKey().defaultRandom(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow().$onUpdate(() => new Date()),
@@ -20,7 +20,7 @@ export const chirps = pgTable("chirps", {
   userId: uuid("user_id").notNull().references(() => users.id, {
     onDelete: "cascade",
   }),
-});
+});*/
 
 export const refreshTokens = pgTable("refresh_tokens", {
   token: varchar("token", { length: 64 }).primaryKey(),
@@ -37,5 +37,5 @@ export type RefreshToken = typeof refreshTokens.$inferSelect;
 export type NewRefreshToken = typeof refreshTokens.$inferInsert;
 export type NewUser = typeof users.$inferInsert;
 export type User = typeof users.$inferSelect;
-export type NewChirp = typeof chirps.$inferInsert;
-export type Chirp = typeof chirps.$inferSelect;
+//export type NewChirp = typeof chirps.$inferInsert;
+//export type Chirp = typeof chirps.$inferSelect;
